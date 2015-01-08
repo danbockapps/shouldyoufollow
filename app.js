5bd1d09d450c9f7db379d94e42aad61dfe9cc1ab
Dan Bock <danbock@gmail.com>
2015-01-07 22:07:05 -0500

    Show one tweet at a time

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ function MainCtrl($scope, $http) {
     $http.get("api.php?screen_name=" + $scope.screenName).success(function(data) {
       console.log(data);
       $scope.tweets = data;
+      $scope.iterator = 0;
+
+      for(var key in $scope.tweets) {
+        $scope.tweets[key].key = key;
+      }
+
       $.getScript("//platform.twitter.com/widgets.js");
     });  
-  };  
+  };
+
+
+  $scope.nextTweet = function() {
+    $scope.iterator++;
+    console.log("iterator is now " + $scope.iterator);
+  }
 }
 
 app.controller("MainCtrl", MainCtrl);
